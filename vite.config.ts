6e330ce3b7efe96fff9f18b1eef7e22f949fc523
Raid Aboul Hosn <raidhosn1972@gmail.com>
2025-11-30 08:29:39 -0800

    commit
    commit

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Table_Parser_Claude_v1/',
-})
