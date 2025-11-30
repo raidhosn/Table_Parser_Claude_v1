ff27699794303bc1e3c75691ab3498291e68221e
Raid Aboul Hosn <raidhosn1972@gmail.com>
2025-11-30 09:09:28 -0800

    commit
    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Table_Parser_Claude_v1/',
+  base: '/Table_Parser_Claude_v1/',  // <-- This is the fix
+  build: {
+    outDir: 'docs'
+  }
 })
